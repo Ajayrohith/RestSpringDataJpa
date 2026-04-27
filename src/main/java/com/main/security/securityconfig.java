11bcb15390568e5b3721ee32b8647ef9aac56558
Ajayrohith <ajayrohith11@gmail.com>
2026-04-27 19:13:54 +0530

    Commit from personal 27 april 19.20

--- src/main/java/com/main/security/securityconfig.java
+++ src/main/java/com/main/security/securityconfig.java
@@ -2,9 +2,13 @@ package com.main.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class securityconfig {
@@ -33,6 +37,22 @@ public class securityconfig {
         
         return new InMemoryUserDetailsManager(Ajay,Ajeet,Susan);
         
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+    {
+        http.authorizeHttpRequests(configurer
+            -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("Employee")
+            .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("Employee")
+            .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("Manager")
+            .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("Admin")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
     }
 
 }
