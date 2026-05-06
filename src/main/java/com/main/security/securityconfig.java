64e861f1d2a81a87757896d4786db3fd6c1d22e9
Ajayrohith <ajayrohith11@gmail.com>
2026-05-06 22:53:50 +0530

    Commited from Lap 6th MAY

--- src/main/java/com/main/security/securityconfig.java
+++ src/main/java/com/main/security/securityconfig.java
@@ -1,5 +1,7 @@
 package com.main.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,45 +10,60 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+
 
 @Configuration
 public class securityconfig {
 
+    // We use the below InMemoryUserDetailsManager for hardcoding usernames,passwords and roles
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager()
+    // {
+
+    //     UserDetails Ajay = User.builder()
+    //                         .username("Ajay")
+    //                         .password("{noop}test123")
+    //                         .roles("Employee")
+    //                         .build();
+
+    //     UserDetails Ajeet = User.builder()
+    //                         .username("Ajeet")
+    //                         .password("{noop}test456")
+    //                         .roles("Employee","Manager")
+    //                         .build();
+
+    //     UserDetails Susan = User.builder()
+    //                         .username("Susan")
+    //                         .password("{noop}test456")
+    //                         .roles("Employee","Manager","Admin")
+    //                         .build();
+        
+    //     return new InMemoryUserDetailsManager(Ajay,Ajeet,Susan);
+        
+    // }
+
+
+    //The Below userDetailsManager is used for retrieving user and priviages from Db 
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager()
+    public UserDetailsManager userDetailsManager (DataSource datasource)
     {
-
-        UserDetails Ajay = User.builder()
-                            .username("Ajay")
-                            .password("{noop}test123")
-                            .roles("Employee")
-                            .build();
-
-        UserDetails Ajeet = User.builder()
-                            .username("Ajeet")
-                            .password("{noop}test456")
-                            .roles("Employee","Manager")
-                            .build();
-
-        UserDetails Susan = User.builder()
-                            .username("Susan")
-                            .password("{noop}test456")
-                            .roles("Employee","Manager","Admin")
-                            .build();
-        
-        return new InMemoryUserDetailsManager(Ajay,Ajeet,Susan);
-        
+        return new JdbcUserDetailsManager(datasource);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
     {
         http.authorizeHttpRequests(configurer
-            -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("Employee")
-            .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("Employee")
-            .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("Manager")
-            .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("Admin")
+            -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+            .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+            .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+            .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
 
         http.httpBasic(Customizer.withDefaults());
